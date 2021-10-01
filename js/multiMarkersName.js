e4d9cbd372428eb71e6fb855948695eb89e673d7
Wu Justin <justin.cathay@gmail.com>
2021-10-01 23:43:15 +0800

    add multi markers

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -31,3 +31,17 @@ AFRAME.registerComponent('markers_start',{
     }
   }
 });
+
+AFRAME.registerComponent('registerevents', {
+  init: function () {
+    const marker = this.el;
+    marker.addEventListener("markerFound", ()=> {
+      var markerId = marker.id;
+      console.log('Marker Found: ', markerId);
+    });
+    marker.addEventListener("markerLost",() =>{
+      var markerId = marker.id;
+      console.log('Marker Lost: ', markerId);
+    });
+  },
+});
